2e0a8058afe45f3f739815df96b08dc8a845bebf
ohminimarie <mcjalcantara@gmail.com>
2016-09-12 01:51:02 -0700

    Notes on variables

--- lecture73/script.js
+++ lecture73/script.js
@@ -70,3 +70,20 @@ null
 //Variables are simply containers that store values
 //They follow this pattern:
 var yourVariableName = yourValue;
+
+//They can store all of the values we've seen
+var name = "Rusty";
+var secretNumber = 73;
+var isAdorable = true;
+
+//Recall the store value by calling the variable name
+var name = "Rusty";
+"hello there " + name     //"hello there Rusty"
+
+var num = 37;
+num + 3 + 10      //50
+
+//We can also update existing variables
+var name = "Robert";
+name = "Bob";
+
