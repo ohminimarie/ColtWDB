85b21e0eccf1fde75069b54fb6c29eb0877a29e7
ohminimarie <mcjalcantara@gmail.com>
2016-09-21 11:27:39 -0700

    Created notes and example for conditionals

---
+++ lecture87/conditionals.js
@@ -0,0 +1,17 @@
+//if
+//If you are younger than 18 
+//you cannot enter the venue
+//elseif
+//If you are between 18 & 21
+//you can enter but cannot drink
+//else
+//you can enter and drink
+
+if(age < 18) {
+  console.log("Sorry, you are not old enough to enter the venue.");
+} else if(age < 21) {
+  console.log("You can enter, but cannot drink");
+} else (age > 21) {
+  console.log("You can enter and drink");
+}
+
