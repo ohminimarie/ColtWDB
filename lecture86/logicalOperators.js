45444f88203450a0cc8c382b31a3d76fd77e88e4
ohminimarie <mcjalcantara@gmail.com>
2016-09-21 11:18:11 -0700

    Added notes from lecture

--- lecture86/logicalOperators.js
+++ lecture86/logicalOperators.js
@@ -8,3 +8,37 @@ x > 9 || x === 5 //true
 // ! NOT (requires to be false)
 !(x===y)  //true
 
+// Exercise 1
+
+var x = 10;
+var y = "a"
+
+y === "b" || x >= 10 //true
+
+// Exercise 2
+
+var x = 3;
+var y = 8;
+
+!(x == "3" || x === y) && !(y != 8 && x <= y) //false
+
+!"Hello World" //false
+!"" //false
+!!"hello" //true
+!!"" //false
+!!null //false
+!!0 //false
+!!NaN //false
+!!-1 //true
+
+
+//Falsy Values
+// false
+// 0
+// ""
+// null
+// undefined
+// NaN
+// Everything else is truthy
+
+//
