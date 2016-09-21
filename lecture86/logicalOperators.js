47219d29b276ff19fccfbce843d5232f81eafa4f
ohminimarie <mcjalcantara@gmail.com>
2016-09-21 11:08:20 -0700

    Created examples for logical operators

---
+++ lecture86/logicalOperators.js
@@ -0,0 +1,10 @@
+// assuming x=5 and y=9
+// && AND (requires both sides to be true)
+x < 10 && x !== 5 //false
+
+// || OR (requires one side to be true)
+x > 9 || x === 5 //true
+
+// ! NOT (requires to be false)
+!(x===y)  //true
+
