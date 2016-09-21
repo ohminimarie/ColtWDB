9175e57bb206fcf7ce1a218f25168a0eb418d759
ohminimarie <mcjalcantara@gmail.com>
2016-09-21 11:53:25 -0700

    Adding logic and refactoring

--- lecture89/game.js
+++ lecture89/game.js
@@ -1,1 +1,21 @@
-alert("HELLO");
+// alert("HELLO");
+
+//create secretNumber
+var secretNumber = 4;
+
+//ask user for guess
+var guess = Number(prompt("Guess a number"));
+// alert(guess);
+
+//check if guess is right
+if (guess === secretNumber) {
+  alert("YOU GOT IT RIGHT!");
+} 
+//check if guess is higher
+else if(guess > secretNumber) {
+  alert("Too high. Guess again.");
+}
+//otherwise, check if lower
+else {
+  alert("Too low. Guess again.");
+}
