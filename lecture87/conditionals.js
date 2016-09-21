ee4d860906a668978725cff2922f267b14d0514e
ohminimarie <mcjalcantara@gmail.com>
2016-09-21 11:33:43 -0700

    Working on exercise

--- lecture87/conditionals.js
+++ lecture87/conditionals.js
@@ -7,11 +7,21 @@
 //else
 //you can enter and drink
 
-if(age < 18) {
-  console.log("Sorry, you are not old enough to enter the venue.");
-} else if(age < 21) {
-  console.log("You can enter, but cannot drink");
-} else {
-  console.log("You can enter and drink");
-}
+// if(age < 18) {
+//   console.log("Sorry, you are not old enough to enter the venue.");
+// } else if(age < 21) {
+//   console.log("You can enter, but cannot drink");
+// } else {
+//   console.log("You can enter and drink");
+// }
 
+
+if (age < 0) {
+  console.log("Sorry, that's not a real age");
+} else if(age === 21) {
+  console.log("Happy 21st Birthday!!");
+} else if(age % 2 !== 0) {
+  console.log("Your age is odd!");
+} else if(age) {
+  console.log("Perfect Square!");
+}
