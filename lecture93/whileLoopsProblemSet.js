9291077b618e2f2015728e8b09b596b1db22d251
ohminimarie <mcjalcantara@gmail.com>
2016-11-02 13:44:24 -0700

    Added and completed ex's 1 and 2

---
+++ lecture93/whileLoopsProblemSet.js
@@ -0,0 +1,27 @@
+// While Loops Problem Set
+// Exercise 1
+// Print all numbers between -10 and 19
+var num = -10;
+
+while(num <= 19) {
+  console.log(num);
+  num++;
+}
+
+// Exercise 2
+// Print all even numbers between 10 and 40
+
+var num = 10;
+
+while(num <= 40){
+  if(num % 2 === 0){
+    console.log(num);
+  }
+  num++;
+}
+
+// Exercise 3
+// Print all odd numbers between 300 and 333
+
+// Exercise 4
+// Print all numbers divisible by 5 and 3 between 5 and 50
