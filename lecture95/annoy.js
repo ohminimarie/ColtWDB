274aa754c612c500c49364e32026f5f26da0970a
ohminimarie <mcjalcantara@gmail.com>
2016-11-02 15:44:39 -0700

    Completed code-a-long

--- lecture95/annoy.js
+++ lecture95/annoy.js
@@ -1,1 +1,7 @@
-alert("Connected!");
+var answer = prompt("Are we there yet?");
+
+while (answer !== "yes" && answer !== "yeah" && answer.indexOf("yes") === -1){
+  var answer = prompt("Are we there yet?");
+}
+
+alert("YAY, WE MADE IT!!!");
