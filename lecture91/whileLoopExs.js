dc14580ef3a10c88b911af004df44f9dd59ada88
ohminimarie <mcjalcantara@gmail.com>
2016-11-02 13:30:22 -0700

    Completed Ex. 2

--- lecture91/whileLoopExs.js
+++ lecture91/whileLoopExs.js
@@ -10,3 +10,15 @@ while(num <= 10) {
   num += 2;
 }
 
+// Exercise 2
+// While num is less than or 20, if it is a multiple of 4 it will
+// print out the number. Expression will evaluate at 20.
+
+var num = 1;
+
+while(num <= 20) {
+  if(num % 4 === 0) {
+    console.log(num);
+  }
+  num++;
+}
