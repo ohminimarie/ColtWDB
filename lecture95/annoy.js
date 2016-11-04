01aead791bfb2ebf360472aea3e3cf53d07b7f2b
ohminimarie <mcjalcantara@gmail.com>
2016-11-04 14:45:44 -0700

    Finished exercises

--- lecture95/annoy.js
+++ lecture95/annoy.js
@@ -1,7 +1,7 @@
 var answer = prompt("Are we there yet?");
 
 while (answer !== "yes" && answer !== "yeah" && answer.indexOf("yes") === -1){
-  var answer = prompt("Are we there yet?");
+  answer;
 }
 
 alert("YAY, WE MADE IT!!!");
