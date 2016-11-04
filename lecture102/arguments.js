ac5197d756bb23e0b9cf1facc3182d5fc18338e7
ohminimarie <mcjalcantara@gmail.com>
2016-11-04 15:22:51 -0700

    Finished notes for Arguments.js lecture

--- lecture102/arguments.js
+++ lecture102/arguments.js
@@ -1,7 +1,32 @@
 function square(num) {
   console.log(num * num);
 }
-
 square(10); //Prints out 100
 square(3); //Prints out 9
 square(4); //Prints out 16
+
+
+function sayHello(){
+  console.log("Hello there!");
+}
+sayHello();
+
+
+function sayHello(name){
+  console.log("Hello there," + name + "!");
+}
+sayHello("Rusty");
+
+
+function area(length, width) {
+  console.log(length * width);
+}
+area(9,2);
+
+
+function greet(person1, person2, person3) {
+  console.log("hi " + person1);
+  console.log("hi " + person2);
+  console.log("hi " + person3);
+}
+greet("Harry", "Ron", "Hermione");
