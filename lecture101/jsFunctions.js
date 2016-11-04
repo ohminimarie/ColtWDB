42b65c3c15e0366f41a72b469e5f4fa2c3a64fa6
ohminimarie <mcjalcantara@gmail.com>
2016-11-04 16:33:00 -0700

    Saved last changes

--- lecture101/jsFunctions.js
+++ lecture101/jsFunctions.js
@@ -9,3 +9,14 @@ doSomething();
 doSomething();
 doSomething();
 doSomething();
+
+function singSong() {
+  console.log("Twinkle, twinkle, little star");
+  console.log("How I wonder what you are");
+  console.log("Up above the world so high,");
+  console.log("Like a diamond in the sky.");
+};
+
+singSong();
+singSong();
+singSong();
