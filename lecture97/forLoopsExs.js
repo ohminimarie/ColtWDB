0d68dfe3688714b1cee78a2fffd13c8ce81de966
ohminimarie <mcjalcantara@gmail.com>
2016-11-04 12:16:25 -0700

    Combining lecture98: Loops Problem set with forLoopsEx.js

---
+++ lecture97/forLoopsExs.js
@@ -0,0 +1,16 @@
+// Console.logs 0 and 8
+for(var i=0; i < 16; i+= 8) {
+  console.log(i);
+}
+
+// Console.logs h, e, l , l, o
+var str = "ahceclwlxo";
+
+for(var i = 1; i < str.length; i+=2) {
+  console.log(str[i]);
+}
+
+// Print all numbers between -10 and 19
+// Print all even numbers between 10 and 40
+// Print all odd numbers between 300 and 333
+// Print all numbers divisible by 5 and 3 between 5 and 50
