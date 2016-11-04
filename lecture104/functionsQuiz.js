eec6c8d6365fef2b679aa1c5b70e711354804df6
ohminimarie <mcjalcantara@gmail.com>
2016-11-04 16:31:44 -0700

    Finished example quiz problems'

--- lecture104/functionsQuiz.js
+++ lecture104/functionsQuiz.js
@@ -5,3 +5,16 @@ function test(x,y) {
 }
 
 test(10, 40);
+
+//returns 1600 only. first return exits (execution of a function stops) 
+// out of expression, so the rest of the code (console.log & return x/2)
+// will not be run
+function test(x) {
+  return x*2;
+  console.log(x);
+  return x/2;
+}
+
+test(40)
+
+//
