a05fc1f4eb9b14c9778769c109b16a5d546d630c
ohminimarie <mcjalcantara@gmail.com>
2016-11-04 20:40:12 -0700

    Creating notes on array methods lecture

---
+++ lecture112/arrayMethods.js
@@ -0,0 +1,12 @@
+// use push to add to the end of an array
+var colors = ["red", "orange", "yellow"];
+colors.push("green");
+//["red", "orange", "yellow", "green"]
+
+//use pop to remove the last item in an array
+var colors = ["red", "orange", "yellow"];
+colors.pop();
+//["red", "orange"]
+
+//pop() returns the removed element
+var col = colors.pop(); //orange
