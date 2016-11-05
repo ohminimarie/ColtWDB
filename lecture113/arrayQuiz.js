8cba31eaea7615c40e3de8f4ee0374994a275c0a
ohminimarie <mcjalcantara@gmail.com>
2016-11-04 21:01:31 -0700

    Created and finished arrayQuiz

---
+++ lecture113/arrayQuiz.js
@@ -0,0 +1,16 @@
+//Exercise 1
+var numbers = [22, 67, 33, 96, 88];
+//What does the following line print?
+console.log(numbers[numbers.length]);
+//will print undefined
+
+//Exercise 2
+var friendGroups = [
+  ["Harry", "Ron", "Hermione"],
+  ["Malfoy", "Crabbe", "Goyle"],
+  ["Mooney", "Wormtail", "Prongs"]
+];
+
+//What is the result of this line:
+console.log(friendGroups[2][0]);
+//"Mooney"
