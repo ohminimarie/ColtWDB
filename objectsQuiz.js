dffbac17c9edde42728e0441c51e257356033c49
ohminimarie <mcjalcantara@gmail.com>
2016-12-10 14:54:42 -0800

    Completed problem 1

---
+++ objectsQuiz.js
@@ -0,0 +1,14 @@
+//Objects
+//Exercise 1
+
+var someObject = {};
+
+//Which of the following are valid:
+someObject._name = "Hedwig";  //valid
+
+someObject.age = 6; //valid
+
+var prop = "color"
+someObject[prop] = "red"; //valid
+
+someObject.123 = true;  //invalid
