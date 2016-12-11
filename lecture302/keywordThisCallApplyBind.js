fccc234b3dee0f2aa00a76cb7b17577548fef4d8
ohminimarie <mcjalcantara@gmail.com>
2016-12-11 01:31:52 -0800

    Finished lecture 302 notes

---
+++ lecture302/keywordThisCallApplyBind.js
@@ -0,0 +1,7 @@
+//Explicit Binding
+//Choose what we want the context of 'this' to be using call, apply, or bind
+
+Name of Method      Parameters              Invoke Immediately?
+Call                thisArg, a, b, c, d     Yes
+Apply               thisArg, [a,b,c,d]      Yes
+Bind                thisArg, a, b, c, d     No
