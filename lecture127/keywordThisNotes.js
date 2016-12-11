16192ce8695de872a3335bbd977a15c5dd03391f
ohminimarie <mcjalcantara@gmail.com>
2016-12-10 22:58:46 -0800

    Created notes on keyword this

---
+++ lecture127/keywordThisNotes.js
@@ -0,0 +1,10 @@
+var comments = {};
+
+comments.data = ["Good Job!", "Bye", "Lame..."];
+
+//to print out each comment - created a comments method
+comments.print = function(){
+  this.data.forEach(function(){
+    console.log(el);
+  });
+}
