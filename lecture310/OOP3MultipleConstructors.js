89319c0de41a603fe03e029ab8980e84ee3af1d6
ohminimarie <mcjalcantara@gmail.com>
2016-12-11 16:07:46 -0800

    Using call and apply in multiple constructor

--- lecture310/OOP3MultipleConstructors.js
+++ lecture310/OOP3MultipleConstructors.js
@@ -2,7 +2,7 @@
 //Let's create two constructor functions, one for a Car and
 //one for a Motorcycle - here is what it might look like
 
-function Car(make,model, year) {
+function Car(make, model, year) {
   this.make = make;
   this.model = model;
   this.year = year;
@@ -21,3 +21,31 @@ function Motorcycle(make, model, year){
 //Notice how much duplication is going on in the motorcycle function. 
 //Is there anyway to "borrow" the Car function and invoke it inside 
 //the Motorcycle function?
+
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  //we can also set properties on the keyword this
+  //that are preset values
+  this.numWheels = 4;
+}
+
+function Motorcycle(make, model, year){
+  //using Call
+  Car.call(this, make, model, year)
+  this.numWheels = 2;
+}
+// OR
+
+function Motorcycle(make, model, year){
+  //using Call
+  Car.call(this, [make, model, year])
+  this.numWheels = 2;
+}
+
+function Motorcycle(make, model, year){
+  //using Call
+  Car.call(this, arguments)
+  this.numWheels = 2;
+}
