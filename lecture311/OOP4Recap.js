798bbde65ff04ad2c39c6f8bda1649b9553afc9e
ohminimarie <mcjalcantara@gmail.com>
2016-12-11 16:15:04 -0800

    Recapped OOP Lecture notes

---
+++ lecture311/OOP4Recap.js
@@ -0,0 +1,15 @@
+//Recap
+
+//OOP is a model based on objects constructed from a blueprint. We use
+//OOP to write more modular and shareable code
+
+//In languages that have built-in support for OOP, we call these 
+//blueprints "classes" and the objects created from them "instances"
+
+//Since we do not have built-in class support in Javascript, we mimic
+//classes by using fnctions. These constructor functions create objects
+//through the use of the new keyword
+
+
+//We can avoide duplication in multiple constructor funtions by using call 
+//or apply
