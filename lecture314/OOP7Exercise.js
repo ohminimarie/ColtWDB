de644fe69335571bd04446eb6e9724cdb0e483f7
ohminimarie <mcjalcantara@gmail.com>
2016-12-11 17:04:39 -0800

    Comparing answers and recap

--- lecture314/OOP7Exercise.js
+++ lecture314/OOP7Exercise.js
@@ -28,4 +28,41 @@ Person.prototype.sayHi = function(){
 elie = new Person("Elie");
 elie.sayHi(); //Hi Elie
 
+//Your turn!
+
+//Create a constructor function for a Vehicle: every object created
+//from this constructor should have a make, model, and year property.
+//Each object should also have a property called isRunning, which
+//should be set to false
+
+function Vehicle(make, model, year){
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.isRunning = false;
+}
+
+//Every object created from the Vehicle constructor should have a
+//function called turnOn, which changes the isRunning property to true
+
+//Every object created from the Vehicle constructor should have a
+//function called turnOff, which changes the isRunning property to false
+
+//Every object created from the Vehicle constructor should have a method
+//called honk, which returns the string "beep" ONLY if the isRunning
+//property is true
+
+Vehicle.prototype.turnOn = function(){
+  this.isRunning = true;
+}
+
+Vehicle.prototype.turnOff = function(){
+  this.isRunning = false;
+}
+
+Vehicle.prototype.honk = function(){
+  if(this.isRunning){
+    return "beep";
+  }
+}
 
