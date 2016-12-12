300622e4dbd7caa86c9a4bfb593b4bea47197c70
ohminimarie <mcjalcantara@gmail.com>
2016-12-11 17:07:53 -0800

    Finished lecture notes and comparing solutino

--- lecture314/OOP7Exercise.js
+++ lecture314/OOP7Exercise.js
@@ -66,3 +66,16 @@ Vehicle.prototype.honk = function(){
   }
 }
 
+
+// RECAP
+
+// Every time the new keyword is used, a link between the object
+// created and the prototype property of the constructor is
+// established - this link can be accessed using __proto__
+
+// The prototype object contains a property called constructor,
+// which points back to the constructor function
+
+// To share properties and methods for objects created by a 
+// constructor function, place them in the prototype as it is 
+// the most efficient
